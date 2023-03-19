c27308cad9774cabcfb4fa647336e89fca37ff0f
Javad Fathy <jwadfthy2@gmail.com>
2023-03-19 23:30:02 +0330

    initial

---
+++ models/products.js
@@ -0,0 +1,60 @@
+const products = [
+    {
+        _id: 1,
+        name: 'Airpad 1254',
+        image: '/images/prd1.jpg',
+        price: 12000,
+        colors: ['#d8d82b', '#23b110', '#102db1'],
+        discription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+    },
+    {
+        _id: 2,
+        name: 'Airpad 657',
+        image: '/images/prd2.jpg',
+        price: 12000,
+        colors: ['#d8d82b', '#23b110', '#102db1'],
+        discription: 'Officiis, eos nulla. Non ad doloribus earum provident natus. Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit'
+    },
+    {
+        _id: 3,
+        name: 'Airpad 247',
+        image: '/images/prd3.jpg',
+        price: 12000,
+        colors: ['#d8d82b', '#23b110', '#102db1'],
+        discription: 'Nesciunt, aut, quod rerum commodi, officiis quos iure magni perferendis eligendi hic facilis? Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit'
+    },
+    {
+        _id: 4,
+        name: 'Airpad 432',
+        image: '/images/prd4.jpg',
+        price: 12000,
+        colors: ['#d8d82b', '#23b110', '#102db1'],
+        discription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit'
+    },
+    {
+        _id: 5,
+        name: 'Airpad 987',
+        image: '/images/prd5.jpg',
+        price: 12000,
+        colors: ['#d8d82b', '#23b110', '#102db1'],
+        discription: 'Nesciunt, aut, quod rerum commodi, officiis quos iure magni perferendis eligendi hic facilis?. Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit'
+    },
+    {
+        _id: 6,
+        name: 'Airpad 124',
+        image: '/images/prd6.jpg',
+        price: 12000,
+        colors: ['#d8d82b', '#23b110', '#102db1'],
+        discription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit'
+    },
+    {
+        _id: 7,
+        name: 'Airpad 658',
+        image: '/images/prd7.jpg',
+        price: 12000,
+        colors: ['#d8d82b', '#23b110', '#102db1'],
+        discription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit'
+    },
+]
+
+export default products
